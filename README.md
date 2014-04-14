1a56d54ed0dd12f8a0f3a076efbe7aa84812a4c8
CamShaft <bytheway.cameron@gmail.com>
2014-04-14 14:56:19 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 ng-alias
 ========
 
-An angular directive to alias a variable
+An angular directive to alias a variable.
+
+Usage
+-----
+
+A one-way alias is established between the source and the destination. In this example `input` is aliased to `model`. Any changes that happen to `input` will be propogated.
+
+```html
+<input data-ng-model="input" data-ng-alias="model = input" />
+```
+
+This allows for reaching outside of the scope.
+
+```html
+<input data-ng-model="input" data-ng-alias="$parent.model = input" />
+```
+
+Multiple aliased can be registered on the same element
+
+```html
+<div data-ng-alias="alias1 = input1, $root.alias2 = input2">
+  <input data-ng-model="input1" />
+  <input data-ng-model="input2" />
+</div>
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Module dependencies
+ */
+
+var angular = window.angular;
+
+var name = 'ng-alias';
+
+var pkg = module.exports = angular.module(name, []);
+
+pkg.name = name;
+
+pkg.directive('ngAlias', [
+  function() {
+    return {
+      link: function($scope, elem, attrs) {
+        var aliases = attrs.ngAlias.split(',');
+        angular.forEach(aliases, function(alias) {
+          var parts = alias.split('=');
+          var path = parts[0].trim().split('.');
+          $scope.$watch(parts[1].trim(), function(val) {
+            set(path, val, $scope, 0);
+          });
+        });
+      }
+    };
+  }
+]);
+
+function set(path, val, scope, i) {
+  var key = path[i];
+  if (i === path.length - 1) return scope[key] = val;
+  if (angular.isUndefined(scope[key])) scope[key] = {};
+  return set(path, val, scope[key], i + 1);
+}
